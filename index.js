9013de69df0690d9a02989f78706a8345bc1fab7
TheRealKevBot <Kglas001@gmail.com>
2021-06-16 14:50:42 -0600

    Build

--- index.js
+++ index.js
@@ -4,21 +4,27 @@
 
 
 function wave(str){
-    // Code here
+    let i = 0
+    let result = []
+    while (i < str.length) {
+        str[i] === ' ' ? null : result.push(str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1) )
+        i++
+    }
+    return result
   } 
 
 
 result1 = ["Hello", "hEllo", "heLlo", "helLo", "hellO"];
-console.log(wave("hello"), "Should return:" + result1);
+console.log(wave("hello"), "Should return: ", result1);
 
 result2 = ["Codewars", "cOdewars", "coDewars", "codEwars", "codeWars", "codewArs", "codewaRs", "codewarS"];
-console.log(wave("codewars"), "Should return:" + result2);
+console.log(wave("codewars"), "Should return: ", result2);
 
 result3 = [];
-console.log(wave(""), "Should return:" + result3);
+console.log(wave(""), "Should return: ", result3);
 
 result4 = ["Two words", "tWo words", "twO words", "two Words", "two wOrds", "two woRds", "two worDs", "two wordS"];
-console.log(wave("two words"), "Should return:" + result4);
+console.log(wave("two words"), "Should return: ", result4);
 
 result5 = [" Gap ", " gAp ", " gaP "];
-console.log(wave(" gap "), "Should return:" + result5);
+console.log(wave(" gap "), "Should return: ", result5);
